62d7d70c2772ad1a7a8deefc1441dc4e29d6f9d8
Rulee <rullyyustafnugroho@gmail.com>
2022-03-09 21:22:11 +0700

    get campaign detail endpoint

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,5 +1,9 @@
 package campaign
 
+import (
+	"strings"
+)
+
 type CampaignFormatter struct {
 	ID               int    `json:"id"`
 	UserID           int    `json:"user_id"`
@@ -39,4 +43,76 @@ func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
 	}
 
 	return CampaignsFormatter
+}
+
+type CampaignDetailFormatter struct {
+	ID               int                       `json:"id"`
+	Name             string                    `json:"name"`
+	ShortDescription string                    `json:"short_description"`
+	Description      string                    `json:"description"`
+	ImageURL         string                    `json:"image_url"`
+	GoalAmount       int                       `json:"goal_amount"`
+	CurrentAmount    int                       `json:"current_amount"`
+	UserID           int                       `json:"user_id"`
+	Slug             string                    `json:"slug"`
+	Perks            []string                  `json:"perks"`
+	User             CampaignUserFormatter     `json:"user"`
+	Images           []CampaignImagesFormatter `json:"campaign_images"`
+}
+
+type CampaignUserFormatter struct {
+	Name     string `json:"name"`
+	ImageURL string `json:"image_rul"`
+}
+
+type CampaignImagesFormatter struct {
+	ImageURL  string `json:"image_url"`
+	IsPrimary bool   `json:"is_primary"`
+}
+
+func FormatCampaignDetail(campaign Campaign) CampaignDetailFormatter {
+	perks := []string{}
+
+	for _, perk := range strings.Split(campaign.Perks, ",") {
+		perks = append(perks, strings.TrimSpace(perk))
+	}
+
+	CampaignUserFormatter := CampaignUserFormatter{
+		Name:     campaign.User.Name,
+		ImageURL: campaign.User.AvatarFileName,
+	}
+
+	images := []CampaignImagesFormatter{}
+	for _, image := range campaign.CampaignImages {
+		campaignImagesFormatter := CampaignImagesFormatter{}
+		campaignImagesFormatter.ImageURL = image.FileName
+
+		isPrimary := false
+		if image.IsPrimary == 1 {
+			isPrimary = true
+		}
+		campaignImagesFormatter.IsPrimary = isPrimary
+		images = append(images, campaignImagesFormatter)
+	}
+
+	CampaignDetailFormatter := CampaignDetailFormatter{
+		ID:               campaign.ID,
+		UserID:           campaign.UserID,
+		Name:             campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		Description:      campaign.Description,
+		ImageURL:         "",
+		Slug:             campaign.Slug,
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+		Perks:            perks,
+		User:             CampaignUserFormatter,
+		Images:           images,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		CampaignDetailFormatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return CampaignDetailFormatter
 }
--- handler/campaign.go
+++ handler/campaign.go
@@ -30,3 +30,24 @@ func (h *CampaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("List of campaigns", http.StatusBadRequest, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusBadRequest, response)
 }
+
+func (h *CampaignHandler) GetCampaign(c *gin.Context) {
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail of campaign1", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail, err := h.Service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail of campaign2", http.StatusBadRequest, "error", err.Error())
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", campaign.FormatCampaignDetail(campaignDetail))
+	c.JSON(http.StatusOK, response)
+}
