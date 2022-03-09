14a4b8b121cd3d8e72bb4b6960e812a87eb7dc6b
Rulee <rullyyustafnugroho@gmail.com>
2022-03-09 08:41:01 +0700

    Campaign repository

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"startup/auth"
+	"startup/campaign"
 	"startup/handler"
 	"startup/helper"
 	"startup/user"
@@ -31,43 +33,17 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaign, err := campaignRepository.FindByUserID(1)
+
+	for _, campaigns := range campaign {
+		fmt.Println(campaigns.CampaignImages[0].FileName)
+		fmt.Println(campaigns)
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
-
-	//token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyfQ.lchslLcaVh4VKxjqEmUGid1VAVMbxtK4XrOuHNGyXNo")
-	//if err != nil {
-	//	fmt.Println("ERROR")
-	//}
-
-	//if token.Valid {
-	//	fmt.Println("VALID")
-	//} else {
-	//	fmt.Println("INVALID")
-	//}
-
-	//userByEmail, err := userRepository.FindByEmail("rully@gmail.com")
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//}
-	//if userByEmail.ID == 0 {
-	//	fmt.Println("User tidak ditemukan")
-
-	//} else {
-	//	fmt.Println(userByEmail.Name)
-	//}
-	//input := user.LoginInput{
-	//	Email:    "nugroho@domain.com",
-	//	Password: "qwe123",
-	//}
-
-	//user, err := userService.Login(input)
-	//if err != nil {
-	//	fmt.Println("Terjadi kesalahan")
-	//	fmt.Println(err.Error())
-	//}
-
-	//fmt.Println(user.Email)
-	//fmt.Println(user.Name)
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
@@ -80,39 +56,7 @@ func main() {
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 	router.Run()
-
-	//fmt.Println("Connection to database is safe")
-
-	//var users []user.User
-	//db.Find(&users)
-
-	//for _, user := range users {
-	//	fmt.Println(user.Name)
-	//	fmt.Println(user.Email)
-	//	fmt.Println("-----------")
-	//}
-
-	//router := gin.Default()
-	//router.GET("/handler", handler)
-	//router.Run()
 }
-
-//func handler(c *gin.Context) {
-//	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-//	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-//	if err != nil {
-//		log.Fatal(err.Error())
-//	}
-
-//	fmt.Println("Connection to database is safe")
-
-//	var users []user.User
-//	db.Find(&users)
-
-//	c.JSON(http.StatusOK, users)
-
-//}
 
 func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
 
